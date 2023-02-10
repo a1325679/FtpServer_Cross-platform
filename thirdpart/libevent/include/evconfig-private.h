f69361f62323adf2a083cb4e766527bad514364b
hczm <1143347424@qq.com>
2023-02-10 05:34:21 -0800

    hczm Init Project

---
+++ thirdpart/libevent/include/evconfig-private.h
@@ -0,0 +1,29 @@
+/*/*******************************************************************************
+**                                                                            **
+**                     Jiedi(China nanjing)Ltd.                               **
+**	               创建：丁宋涛 夏曹俊，此代码可用作为学习参考                **
+*******************************************************************************/
+
+/*****************************FILE INFOMATION***********************************
+**
+** Project       : Libevent C＋＋高并发网络编程
+** Contact       : xiacaojun@qq.com
+**  博客   : http://blog.csdn.net/jiedichina
+**	视频课程 : 网易云课堂	http://study.163.com/u/xiacaojun		
+			   腾讯课堂		https://jiedi.ke.qq.com/				
+			   csdn学院		http://edu.csdn.net/lecturer/lecturer_detail?lecturer_id=961	
+**             51cto学院	http://edu.51cto.com/lecturer/index/user_id-12016059.html	
+** 			   老夏课堂		http://www.laoxiaketang.com 
+**                 
+**  Libevent C＋＋高并发网络编程 课程群 ：1003847950 加入群下载代码和交流
+**   微信公众号  : jiedi2007
+**		头条号	 : 夏曹俊
+**
+*****************************************************************************
+//！！！！！！！！！ Libevent C＋＋高并发网络编程 课程  QQ群：1003847950 下载代码和交流*/
+#if !defined(EVENT_EVCONFIG__PRIVATE_H_) && !defined(__MINGW32__)
+#define EVENT_EVCONFIG__PRIVATE_H_
+
+/* Nothing to see here.  Move along. */
+
+#endif
---
+++ thirdpart/libevent/include/evutil.h
@@ -0,0 +1,62 @@
+/*/*******************************************************************************
+**                                                                            **
+**                     Jiedi(China nanjing)Ltd.                               **
+**	               创建：丁宋涛 夏曹俊，此代码可用作为学习参考                **
+*******************************************************************************/
+
+/*****************************FILE INFOMATION***********************************
+**
+** Project       : Libevent C＋＋高并发网络编程
+** Contact       : xiacaojun@qq.com
+**  博客   : http://blog.csdn.net/jiedichina
+**	视频课程 : 网易云课堂	http://study.163.com/u/xiacaojun		
+			   腾讯课堂		https://jiedi.ke.qq.com/				
+			   csdn学院		http://edu.csdn.net/lecturer/lecturer_detail?lecturer_id=961	
+**             51cto学院	http://edu.51cto.com/lecturer/index/user_id-12016059.html	
+** 			   老夏课堂		http://www.laoxiaketang.com 
+**                 
+**  Libevent C＋＋高并发网络编程 课程群 ：1003847950 加入群下载代码和交流
+**   微信公众号  : jiedi2007
+**		头条号	 : 夏曹俊
+**
+*****************************************************************************
+//！！！！！！！！！ Libevent C＋＋高并发网络编程 课程  QQ群：1003847950 下载代码和交流*/
+/*
+ * Copyright (c) 2007-2012 Niels Provos and Nick Mathewson
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution.
+ * 3. The name of the author may not be used to endorse or promote products
+ *    derived from this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE AUTHOR ``AS IS'' AND ANY EXPRESS OR
+ * IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES
+ * OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED.
+ * IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR ANY DIRECT, INDIRECT,
+ * INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT
+ * NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+ * DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+ * THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF
+ * THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+#ifndef EVENT1_EVUTIL_H_INCLUDED_
+#define EVENT1_EVUTIL_H_INCLUDED_
+
+/** @file evutil.h
+
+  Utility and compatibility functions for Libevent.
+
+  The <evutil.h> header is deprecated in Libevent 2.0 and later; please
+  use <event2/util.h> instead.
+*/
+
+#include <event2/util.h>
+
+#endif /* EVENT1_EVUTIL_H_INCLUDED_ */
